7815c5e491e6ab9e832d3f1464437591d80520ed
DianaSemenova <menestrelia199702@gmail.com>
2024-01-06 17:50:14 +0400

    feat: added Modal Component

---
+++ src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import s from './Modal.module.css';
+
+export default function Modal({ active, setActive, children }) {
+
+    return (
+        <div
+            className={active ? s.modalActive : s.modal}
+            onClick={() => {
+                setActive(false);
+            }}
+        >
+            <div
+                className={active ? s.contentActive : s.content}
+                onClick={(e) => e.stopPropagation()}
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
