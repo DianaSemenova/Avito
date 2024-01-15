ecd4feb85a8537ce42d8738cf921fef9af1f7059
DianaSemenova <menestrelia199702@gmail.com>
2024-01-15 15:20:35 +0400

    added slkeleton to ProfilePersonal

--- src/pages/Profile/ProfilePersonal.jsx
+++ src/pages/Profile/ProfilePersonal.jsx
@@ -2,6 +2,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import s from './Profile.module.css';
 import PageWrapper from '../../components/common/PageWrapper/PageWrapper';
 import ProfileContent from '../../components/profile/ProfileContent/ProfileContent';
@@ -42,34 +44,50 @@ export default function ProfilePersonal() {
     }, [data]);
 
     return (
-        <main>
-            <div className={s.mainContainer}>
-                <div className={s.centerBlock}>
-                    <PageWrapper />
+        <SkeletonTheme color="#333" highlightColor="#f2f1f0">
+            <main>
+                <div className={s.mainContainer}>
+                    <div className={s.centerBlock}>
+                        <PageWrapper />
 
-                    <h2 className={s.heading}>Здравствуйте, {user?.name}!</h2>
-                    <div className={s.mainProfile}>
-                        <div className={s.profileContent}>
-                            <h3 className={s.profileTitle}>
-                                Настройки профиля
-                            </h3>
-                            <div className={s.profilePersonal}>
-                                <ProfileContent
-                                    page="personal"
-                                    data={user && user}
-                                />
+                        <h2 className={s.heading}>
+                            Здравствуйте,{' '}
+                            {adsUser ? (
+                                `${user?.name}!`
+                            ) : (
+                                <Skeleton width={150} height={40} />
+                            )}
+                        </h2>
+                        <div className={s.mainProfile}>
+                            <div className={s.profileContent}>
+                                <h3 className={s.profileTitle}>
+                                    Настройки профиля
+                                </h3>
+                                <div className={s.profilePersonal}>
+                                    <ProfileContent
+                                        page="personal"
+                                        data={user && user}
+                                    />
+                                </div>
                             </div>
                         </div>
+
+                        <Button
+                            classes="btnExit"
+                            onClick={() => handleLogout()}
+                        >
+                            Выход из аккаунта
+                        </Button>
+
+                        <h3 className={s.mainTitle}>Мои товары</h3>
                     </div>
-
-                    <Button classes="btnExit" onClick={() => handleLogout()}>
-                        Выход из аккаунта
-                    </Button>
-
-                    <h3 className={s.mainTitle}>Мои товары</h3>
+                    <Product
+                        data={adsUser}
+                        isLoading={isLoading}
+                        error={error}
+                    />
                 </div>
-                <Product data={adsUser} isLoading={isLoading} error={error} />
-            </div>
-        </main>
+            </main>
+        </SkeletonTheme>
     );
 }
