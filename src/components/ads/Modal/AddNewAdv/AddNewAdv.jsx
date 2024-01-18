99d68c5519055f256e90603cdc405598ca751354
DianaSemenova <menestrelia199702@gmail.com>
2024-01-18 16:37:12 +0400

    feat: implemented Adding NewAdvText

--- src/components/ads/Modal/AddNewAdv/AddNewAdv.jsx
+++ src/components/ads/Modal/AddNewAdv/AddNewAdv.jsx
@@ -1,13 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
 import s from './AddNewAdv.module.css';
 import IconClose from '../../../UI/Icon/IconClose/IconClose';
 import IconRUb from '../../../UI/Icon/IconRUB/IconRUB';
 import Input from '../../../UI/Input/Input';
 import Button from '../../../UI/Button/Button';
+import { useAddNewAdvTextMutation } from '../../../../services/ads';
 
 export default function AddNewAdv({ setActive }) {
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [postNewAdvText] = useAddNewAdvTextMutation();
+
     const handleCloseClick = () => {
         setActive(false);
+    };
+
+    const addNewAdvText = async () => {
+        try {
+            await postNewAdvText({ title, description, price });
+
+            setActive(false);
+            toast.success('Объявление успешно создано!');
+            navigate('/profile-personal');
+        } catch (error) {
+            toast.error(error.message, { className: s.error });
+        }
     };
 
     return (
@@ -24,6 +47,7 @@ export default function AddNewAdv({ setActive }) {
                         type="text"
                         name="name"
                         placeholder="Введите название"
+                        onChange={(e) => setTitle(e.target.value)}
                     />
                 </div>
                 <div className={s.formBlock}>
@@ -35,6 +59,7 @@ export default function AddNewAdv({ setActive }) {
                         cols="auto"
                         rows="10"
                         placeholder="Введите название"
+                        onChange={(e) => setDescription(e.target.value)}
                     />
                 </div>
 
@@ -67,10 +92,19 @@ export default function AddNewAdv({ setActive }) {
                 </div>
                 <div className={`${s.formBlock} ${s.blockPrice}`}>
                     <label className={s.name}>Цена</label>
-                    <Input classes="areaAdvPrice" type="text" name="name" />
+                    <Input
+                        classes="areaAdvPrice"
+                        type="text"
+                        name="name"
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
                     <IconRUb />
                 </div>
-                <Button classes="btnAdv" isDisabled>
+                <Button
+                    classes="btnAdv"
+                    isDisabled={!title}
+                    onClick={() => addNewAdvText()}
+                >
                     Опубликовать
                 </Button>
             </form>
